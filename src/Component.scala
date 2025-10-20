29c10920d658a90288e4e234e3e28495ba401e18
nguyenyou <tunt081295@gmail.com>
2025-10-20 13:59:09 +0700

    case class Component with name,index,hidden params

--- src/Component.scala
+++ src/Component.scala
@@ -2,7 +2,7 @@ package myapp
 
 import com.raquo.laminar.api.L.*
 
-class Component(name: String, index: Int, hidden: Boolean) {
+case class Component(name: String, index: Int, hidden: Boolean) {
   def apply(): HtmlElement = {
     div(s"$index: Component: $name, hidden: $hidden")
   }
