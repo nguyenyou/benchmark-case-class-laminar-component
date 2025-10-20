36bcfbf82042bda704f1e1807be33a26903c4174
nguyenyou <tunt081295@gmail.com>
2025-10-20 13:56:45 +0700

    class Component with name and index param

--- scalajs-modules.js
+++ scalajs-modules.js
@@ -2893,6 +2893,30 @@ var $d_Lmyapp_App = new $TypeData().initClass($c_Lmyapp_App, "myapp.App", ({
   Lmyapp_App: 1
 }));
 /** @constructor */
+function $c_Lmyapp_Component(name, index) {
+  this.Lmyapp_Component__f_name = null;
+  this.Lmyapp_Component__f_index = 0;
+  this.Lmyapp_Component__f_name = name;
+  this.Lmyapp_Component__f_index = index;
+}
+$c_Lmyapp_Component.prototype = new $h_O();
+$c_Lmyapp_Component.prototype.constructor = $c_Lmyapp_Component;
+/** @constructor */
+function $h_Lmyapp_Component() {
+}
+$h_Lmyapp_Component.prototype = $c_Lmyapp_Component.prototype;
+$c_Lmyapp_Component.prototype.apply__Lcom_raquo_laminar_nodes_ReactiveHtmlElement = (function() {
+  var $x_2 = $n($n($m_Lcom_raquo_laminar_api_package$().Lcom_raquo_laminar_api_package$__f_L).div__Lcom_raquo_laminar_tags_HtmlTag());
+  var $x_1 = $m_sr_ScalaRunTime$();
+  var this$1 = $n($m_Lcom_raquo_laminar_api_package$().Lcom_raquo_laminar_api_package$__f_L);
+  var value = ((this.Lmyapp_Component__f_index + ": Component: ") + this.Lmyapp_Component__f_name);
+  var r = $m_Lcom_raquo_laminar_modifiers_RenderableText$().Lcom_raquo_laminar_modifiers_RenderableText$__f_stringRenderable;
+  return $x_2.apply__sci_Seq__Lcom_raquo_laminar_nodes_ReactiveHtmlElement($x_1.wrapRefArray__AO__sci_ArraySeq(new ($d_Lcom_raquo_laminar_modifiers_Modifier.getArrayOf().constr)([$f_Lcom_raquo_laminar_api_Implicits__textToTextNode__O__Lcom_raquo_laminar_modifiers_RenderableText__Lcom_raquo_laminar_nodes_TextNode(this$1, value, r)])));
+});
+var $d_Lmyapp_Component = new $TypeData().initClass($c_Lmyapp_Component, "myapp.Component", ({
+  Lmyapp_Component: 1
+}));
+/** @constructor */
 function $c_Lmyapp_index$package$() {
 }
 $c_Lmyapp_index$package$.prototype = new $h_O();
@@ -5974,87 +5998,6 @@ var $d_jl_StringBuilder = new $TypeData().initClass($c_jl_StringBuilder, "java.l
 }));
 class $c_jl_VirtualMachineError extends $c_jl_Error {
 }
-/** @constructor */
-function $c_Lmyapp_Component(name, index) {
-  this.Lmyapp_Component__f_name = null;
-  this.Lmyapp_Component__f_index = 0;
-  this.Lmyapp_Component__f_name = name;
-  this.Lmyapp_Component__f_index = index;
-}
-$c_Lmyapp_Component.prototype = new $h_O();
-$c_Lmyapp_Component.prototype.constructor = $c_Lmyapp_Component;
-/** @constructor */
-function $h_Lmyapp_Component() {
-}
-$h_Lmyapp_Component.prototype = $c_Lmyapp_Component.prototype;
-$c_Lmyapp_Component.prototype.productIterator__sc_Iterator = (function() {
-  return new $c_s_Product$$anon$1(this);
-});
-$c_Lmyapp_Component.prototype.hashCode__I = (function() {
-  var acc = (-889275714);
-  var hash = acc;
-  acc = $m_sr_Statics$().mix__I__I__I(hash, 604060893);
-  var hash$1 = acc;
-  var x = this.Lmyapp_Component__f_name;
-  var data = $m_sr_Statics$().anyHash__O__I(x);
-  acc = $m_sr_Statics$().mix__I__I__I(hash$1, data);
-  var hash$2 = acc;
-  var data$1 = this.Lmyapp_Component__f_index;
-  acc = $m_sr_Statics$().mix__I__I__I(hash$2, data$1);
-  var hash$3 = acc;
-  return $m_sr_Statics$().finalizeHash__I__I__I(hash$3, 2);
-});
-$c_Lmyapp_Component.prototype.equals__O__Z = (function(x$0) {
-  if ((this === x$0)) {
-    return true;
-  } else if ((x$0 instanceof $c_Lmyapp_Component)) {
-    var x2 = $as_Lmyapp_Component(x$0);
-    return (((this.Lmyapp_Component__f_index === $n(x2).Lmyapp_Component__f_index) && (this.Lmyapp_Component__f_name === $n(x2).Lmyapp_Component__f_name)) && ($n(x2), true));
-  } else {
-    return false;
-  }
-});
-$c_Lmyapp_Component.prototype.toString__T = (function() {
-  return $m_sr_ScalaRunTime$()._toString__s_Product__T(this);
-});
-$c_Lmyapp_Component.prototype.productArity__I = (function() {
-  return 2;
-});
-$c_Lmyapp_Component.prototype.productPrefix__T = (function() {
-  return "Component";
-});
-$c_Lmyapp_Component.prototype.productElement__I__O = (function(n) {
-  if ((n === 0)) {
-    return this.Lmyapp_Component__f_name;
-  }
-  if ((n === 1)) {
-    return this.Lmyapp_Component__f_index;
-  }
-  throw $ct_jl_IndexOutOfBoundsException__T__(new $c_jl_IndexOutOfBoundsException(), ("" + n));
-});
-$c_Lmyapp_Component.prototype.apply__Lcom_raquo_laminar_nodes_ReactiveHtmlElement = (function() {
-  var $x_2 = $n($n($m_Lcom_raquo_laminar_api_package$().Lcom_raquo_laminar_api_package$__f_L).div__Lcom_raquo_laminar_tags_HtmlTag());
-  var $x_1 = $m_sr_ScalaRunTime$();
-  var this$1 = $n($m_Lcom_raquo_laminar_api_package$().Lcom_raquo_laminar_api_package$__f_L);
-  var value = ((this.Lmyapp_Component__f_index + ": Component: ") + this.Lmyapp_Component__f_name);
-  var r = $m_Lcom_raquo_laminar_modifiers_RenderableText$().Lcom_raquo_laminar_modifiers_RenderableText$__f_stringRenderable;
-  return $x_2.apply__sci_Seq__Lcom_raquo_laminar_nodes_ReactiveHtmlElement($x_1.wrapRefArray__AO__sci_ArraySeq(new ($d_Lcom_raquo_laminar_modifiers_Modifier.getArrayOf().constr)([$f_Lcom_raquo_laminar_api_Implicits__textToTextNode__O__Lcom_raquo_laminar_modifiers_RenderableText__Lcom_raquo_laminar_nodes_TextNode(this$1, value, r)])));
-});
-function $as_Lmyapp_Component(obj) {
-  return (((obj instanceof $c_Lmyapp_Component) || (obj === null)) ? obj : $throwClassCastException(obj, "myapp.Component"));
-}
-function $isArrayOf_Lmyapp_Component(obj, depth) {
-  return (!(!(((obj && obj.$classData) && (obj.$classData.arrayDepth === depth)) && obj.$classData.arrayBase.ancestors.Lmyapp_Component)));
-}
-function $asArrayOf_Lmyapp_Component(obj, depth) {
-  return (($isArrayOf_Lmyapp_Component(obj, depth) || (obj === null)) ? obj : $throwArrayCastException(obj, "Lmyapp.Component;", depth));
-}
-var $d_Lmyapp_Component = new $TypeData().initClass($c_Lmyapp_Component, "myapp.Component", ({
-  Lmyapp_Component: 1,
-  s_Equals: 1,
-  s_Product: 1,
-  Ljava_io_Serializable: 1
-}));
 /** @constructor */
 function $c_s_$eq$colon$eq() {
 }
--- src/Component.scala
+++ src/Component.scala
@@ -2,7 +2,7 @@ package myapp
 
 import com.raquo.laminar.api.L.*
 
-case class Component(name: String, index: Int) {
+class Component(name: String, index: Int) {
   def apply(): HtmlElement = {
     div(s"$index: Component: $name")
   }
